9838eb9e5342cbd1434434b09e435a8d693cb375
i.pargunkin <eazy-mf-e@mail.ru>
2023-11-04 08:56:58 +0300

    final1

--- final/conftest.py
+++ final/conftest.py
@@ -1,18 +1,31 @@
-import pytest, yaml
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
+import yaml
+import requests
 
-with open("./testdata.yaml") as f:
-    testdata = yaml.safe_load(f)
-    browser = testdata["browser"]
+with open("testdata.yaml") as f:
+    test_data = yaml.safe_load(f)
+
 
 @pytest.fixture(scope="session")
 def browser():
-    if browser == "chrome":
-        servise = Service(executable_path=ChromeDriverManager().install())
-        options = webdriver.ChromeOptions()
-        driver = webdriver.Chrome(service=servise, options=options)
+    service = Service(executable_path=ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
+
+
+@pytest.fixture()
+def login():
+    response = requests.post(test_data['url'], data={'username': test_data['username'],
+                                                     'password': test_data['password']})
+    response.encoding = 'utf-8'
+    return response.json()['token']
+
+
+@pytest.fixture()
+def get_title_font_size():
+    return "32px"
